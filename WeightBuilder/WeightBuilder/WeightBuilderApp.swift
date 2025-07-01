707ea9b314e41b9402088b41cdbe614073f53b92
Chloe Brown <chloebrown@mac.lan>
2025-06-30 23:57:04 -0600

    create new swift app project

---
+++ WeightBuilder/WeightBuilder/WeightBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightBuilderApp.swift
+//  WeightBuilder
+//
+//  Created by Chloe Brown on 6/30/25.
+//
+
+import SwiftUI
+
+@main
+struct WeightBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeightBuilder/WeightBuilderTests/WeightBuilderTests.swift
@@ -0,0 +1,16 @@
+//
+//  WeightBuilderTests.swift
+//  WeightBuilderTests
+//
+//  Created by Chloe Brown on 6/30/25.
+//
+
+import Testing
+
+struct WeightBuilderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
