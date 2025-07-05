e4bdf762cc83483cf86bf15d9d26b1144e8c281d
Chloe Brown <chloebrown@mac.lan>
2025-07-05 12:02:07 -0600

    create enum for difficulty level

---
+++ WeightBuilder/WeightBuilder/Difficulty.swift
@@ -0,0 +1,14 @@
+//
+//  Difficulty.swift
+//  WeightBuilder
+//
+//  Created by Chloe Brown on 7/5/25.
+//
+
+import Foundation
+
+enum Difficulty: String, Codable {
+    case beginner = "beginner"
+    case intermediate = "intermediate"
+    case advanced = "advanced"
+}
