f0f72db9c462a37e829dd68b26dbd771b9a70d8b
Chloe Brown <chloebrown@mac.lan>
2025-07-05 22:10:08 -0600

    create file for the create plan view

---
+++ WeightBuilder/WeightBuilder/Core/NewWorkoutPlan/CreateNewWorkoutPlanView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateNewWorkoutPlanView.swift
+//  WeightBuilder
+//
+//  Created by Chloe Brown on 7/5/25.
+//
+
+import SwiftUI
+
+struct CreateNewWorkoutPlanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateNewWorkoutPlanView()
+}
