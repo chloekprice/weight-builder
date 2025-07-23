3bf6a9323a98d57b233983ffb32c65d6b5587ddc
Chloe Brown <chloebrown@mac.lan>
2025-07-22 21:25:30 -0600

    refactor exercise card & make dynamic

--- WeightBuilder/WeightBuilder/Core/Exercise/ExerciseCard.swift
+++ WeightBuilder/WeightBuilder/Core/Exercise/ExerciseCard.swift
@@ -8,18 +8,40 @@
 import SwiftUI
 
 struct ExerciseCard: View {
+    var name: String = "Exercise Name"
+    var image: String = "dumbbell"
+    var numSets: Int = 3
+    var numReps: Int = 10
+    
     var body: some View {
         HStack() {
-            Image(systemName: "dumbbell")
-                .resizable()
-                .frame(width: 72, height: 72)
-            VStack(alignment: .leading){
-                Text("Exercise Name")
-                    .font(.title)
-                Text("default sets of defeault reps")
-            }
+            exerciseImage
+            exerciseDetail
             Spacer()
         }.padding(.horizontal)
+    }
+    
+    
+    var exerciseDescription: some View {
+        Text("\(numSets) sets of \(numReps) reps")
+    }
+    
+    var exerciseDetail: some View {
+        VStack(alignment: .leading){
+            exerciseName
+            exerciseDescription
+        }
+    }
+    
+    var exerciseImage: some View {
+        Image(systemName: image)
+            .resizable()
+            .frame(width: 72, height: 72)
+    }
+    
+    var exerciseName: some View {
+        Text(name)
+            .font(.title)
     }
 }
 
